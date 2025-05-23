71f0592bae41872f84081879188c3e12357614f3
Rahul Singh Chauhan <rahulsinghchauhanrj@gmail.com>
2025-05-23 13:11:36 +0530

    Code Corrections made

--- App.jsx
+++ App.jsx
@@ -1,30 +1,13 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// App.js
 
-import HomeScreen from './src/screens/HomeScreen';
-import NotificationsScreen from './src/screens/NotificationsScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
-import SearchScreen from './src/screens/SearchScreen';
-import SettingsScreen from './src/screens/SettingsScreen';
-import EditProfileScreen from './src/screens/EditProfileScreen';
-import BottomTabs from './src/navigation/bottomNavigation/BottomTabs';
-import DrawerNavigator from './src/navigation/dwawerNavigator/DrawerNavigator';
-const Stack = createNativeStackNavigator();
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigator from './src/navigation/stackNavigation/StackNavigator';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Notifications" component={NotificationsScreen} />
-        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
-        <Stack.Screen name="SearchScreen" component={SearchScreen} />
-        <Stack.Screen name="SettingsScreen" component={SettingsScreen} />
-        <Stack.Screen name="EditProfileScreen" component={EditProfileScreen} />
-        <Stack.Screen name="BottomTabs" component={BottomTabs} />
-        <Stack.Screen name="DrawerNavigator" component={DrawerNavigator}/>
-      </Stack.Navigator>
+      <StackNavigator />
     </NavigationContainer>
   );
 };
